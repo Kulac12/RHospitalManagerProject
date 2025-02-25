19515d730319a9490ac903bdfb17f09179ed41db
ekulac <kulacedanur460@gmail.com>
2025-02-25 17:56:09 +0300

    Doctor

---
+++ Business/Concrete/DoctorService.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class DoctorService : IDoctorService
+    {
+        IDoctorRepository _doctorRepository;
+        public DoctorService(IDoctorRepository doctorRepository)
+        {
+            _doctorRepository = doctorRepository;
+        }
+        public void Add(Doctor doctor)
+        {
+            _doctorRepository.Add(doctor);
+        }
+
+        public Doctor GetByIdentityNumber(string identityNumber)
+        {
+            return _doctorRepository.Get(u => u.IdentityNumber == identityNumber);
+        }
+
+        //public List<OperationClaim> GetClaims(User user)
+        //{
+        //    return _patientRepository.GetClaims(user);
+        //}
+    }
+}
